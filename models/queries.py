4f9a0b747a4beb53b9c4e598c878d8b4d79c612e
thiagokunst <61151858+thiagokunst@users.noreply.github.com>
2022-11-01 23:00:43 -0300

    Update queries.py

--- models/queries.py
+++ models/queries.py
@@ -1,6 +1,6 @@
 from models.connection import rpg_db
 
-
+# Lista todos os usuários
 def fetch_all_users():
     try:
         query_cursor = rpg_db.cursor()
@@ -18,6 +18,7 @@ def fetch_all_users():
         print(e)
         return {"status": False, "data": "deu erro carai"}
 
+# Lista usuário por discord_id
 def fetch_user_by_id(discord_id):
     try:
         query_cursor = rpg_db.cursor()
@@ -35,39 +36,7 @@ def fetch_user_by_id(discord_id):
         print(e)
         return {"status": False, "data": "deu erro carai"}
 
-def insert_user(discord_id):
-    try:
-        query_cursor = rpg_db.cursor()
-
-        query = "INSERT INTO tb_users (discord_id) VALUES (%(discord_id)s)"
-        query_params = {'discord_id': discord_id}
-        query_cursor.execute(query, query_params)
-
-        rpg_db.commit()
-
-        return {"status": True, "data": query_cursor.lastrowid}
-    except Exception as e:
-        print(e)
-        return {"status": False, "data": "deu erro carai"}
-
-def insert_character(user_id, name):
-    try:
-        query_cursor = rpg_db.cursor()
-
-        query = "INSERT INTO tb_characters (user_id, name) VALUES (%(user_id)s, %(name)s)"
-        query_params = {'user_id': user_id, 'name': name}
-        query_cursor.execute(query, query_params)
-
-        rpg_db.commit()
-
-        return {"status": True, "data": query_cursor.lastrowid}
-    except Exception as e:
-        default_message = "deu erro carai"
-        if(e.args[0] == 1062):
-            default_message = "Este nome já existe"
-
-        return {"status": False, "data": default_message}
-
+# Lista todos os personagens
 def fetch_all_characters():
     try:
         query_cursor = rpg_db.cursor()
@@ -85,7 +54,7 @@ def fetch_all_characters():
         print(e)
         return {"status": False, "data": "deu erro carai"}
 
-
+# Lista todos os personagens de um usuário buscando por discord_id
 def fetch_all_characters_by_id(discord_id):
     try:
         query_cursor = rpg_db.cursor()
@@ -102,3 +71,40 @@ def fetch_all_characters_by_id(discord_id):
     except Exception as e:
         print(e)
         return {"status": False, "data": "deu erro carai"}
+
+
+# Insere o usuário (caso ainda não esteja cadastrado) e insere o personagem
+def insert_user_and_character(discord_id, name):
+    try:
+        query_cursor = rpg_db.cursor()
+
+        query = "SELECT * FROM tb_users WHERE discord_id = %(discord_id)s"
+        query_params = {'discord_id': discord_id}
+        query_cursor.execute(query, query_params)
+
+        columns = query_cursor.description
+        result = [{columns[index][0]: column for index, column in enumerate(value)} for value in
+                  query_cursor.fetchall()]
+
+        if(len(result) == 0):
+            query = "INSERT INTO tb_users (discord_id) VALUES (%(discord_id)s)"
+            query_params = {'discord_id': discord_id}
+            query_cursor.execute(query, query_params)
+            rpg_db.commit()
+            user_id = query_cursor.lastrowid
+        else:
+            user_id = result[0]['id']
+
+        query = "INSERT INTO tb_characters (user_id, name) VALUES (%(user_id)s, %(name)s)"
+        query_params = {'user_id': user_id, 'name': name}
+        query_cursor.execute(query, query_params)
+        rpg_db.commit()
+
+        return {"status": True, "data": "Personagem cadastrado"}
+
+    except Exception as e:
+        print(e)
+        default_message = "deu erro carai"
+        if (e.args[0] == 1062):
+            default_message = "Este nome já existe"
+        return {"status": False, "data": default_message}
