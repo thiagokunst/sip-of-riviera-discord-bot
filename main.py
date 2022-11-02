5353beac06db0f5b2dff37a0eb10040db3756132
PetrusKyrbb <50366245+PetrusKyrbb@users.noreply.github.com>
2022-11-02 17:54:49 -0300

    char select updated

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+import json
+import re
+
+import discord
+
 import models.queries as queries
 
 import interactions
 
 import settings
 
+
 bot = interactions.Client(
     token=settings.DISCORD_TOKEN,
-    default_scope=966828980999180288
+    default_scope=966828980999180288,
+    intents=interactions.Intents.DEFAULT | interactions.Intents.GUILD_MEMBERS | interactions.Intents.ALL
 )
+
 
 @bot.event
 async def on_ready():
@@ -27,8 +35,10 @@ async def on_ready():
 )
 async def register(ctx:interactions.CommandContext, name: str,):
     if not all(char.isalpha() or char.isspace() for char in name): return await ctx.send("Errou")
-    message = queries.insert_user_and_character(str(ctx.author.id), name)['data']
+    message = queries.insert_user_and_character(str(ctx.author.id), name, {'lvl': '966878470305087507'})['data']
     await ctx.send(message, ephemeral=True)
+    await ctx.author.modify(roles=[966878470305087507])
+    await ctx.author.modify(nick=name)
 
 @bot.command(
     name="charselect",
@@ -38,21 +48,47 @@ async def charselect(ctx: interactions.CommandContext):
     char_list = []
     db_char_list = queries.fetch_all_characters_by_id(str(ctx.author.id))['data']
     for char in db_char_list:
-        char_list.append(interactions.SelectOption(label=char['name'], value=str(char['id'])+","+char['name']))
+        char_list.append(interactions.SelectOption(label=char['name'], value=char['id']))
     select_menu = interactions.SelectMenu(
         custom_id="selectmenu",
         options=char_list,
-
         placeholder="Selecione o seu personagem",
         min_values=1,
         max_values=1,
     )
     await ctx.send(components=select_menu, ephemeral=True)
 
+
+
 @bot.component("selectmenu")
 async def selectchar(ctx: interactions.ComponentContext, user=None):
-    txt = ctx.data.values[0]
-    splittxt = txt.split(",")
-    await ctx.author.modify(nick=splittxt[1])
+    print(ctx.data.values[0])
+    char = queries.fetch_character_by_id(ctx.data.values[0])['data'][0]
+    roles = json.loads(char['roles_id'])
+    role_list = []
+    for roleid in roles.values():
+        role_list.append(int(roleid))
+    await ctx.author.modify(roles=role_list)
+    await ctx.author.modify(nick=char['name'])
+    await ctx.send("sup nigga", ephemeral=True)
+
+@bot.command(
+    name="hero",
+    description="TBD",
+    options=[
+        interactions.Option(
+            name="link",
+            description="TBD",
+            type=interactions.OptionType.STRING,
+            required=True,
+        ),
+    ],
+)
+async def hero(ctx:interactions.CommandContext, link: str):
+    if not bool(re.match("https://www.heroforge.com/load_config.*", link)):
+        await ctx.send("invalido")
+        return
+    queries.insert_hero_link(ctx.author.id, link)
+    await ctx.send("Link cadastrado")
 
 bot.start()
