d7df2bcac4dde42f2365f65196ecd1da4b31d3d2
PetrusKyrbb <50366245+PetrusKyrbb@users.noreply.github.com>
2022-11-01 23:26:18 -0300

    register update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import models.queries
+import models.queries as queries
 
 import interactions
 
@@ -27,8 +27,24 @@ async def on_ready():
 )
 async def register(ctx:interactions.CommandContext, nome: str,):
     user_discord_id = ctx.author.id
+    user_db = queries.fetch_user_by_id(user_discord_id)
+    if(user_db['status']):
+        if(len(user_db['data']) < 1):
+            await register_user(ctx, user_discord_id)
+        await register_char(ctx, user_db['data']['id'])
+    else:
+        await ctx.send('Deu ruim')
+    print()
     await ctx.send(nome, ephemeral=True)
+#aux
+async def register_user(ctx, user_discord_id):
+    db_user_insert = queries.insert_user(user_discord_id)
+    if(db_user_insert['status']):
+        ctx.send('deu bom')
+    else:
+        ctx.send('deu ruim')
+async def register_char(ctx, user_db_id):
+    queries.insert_character(user_db_id)
 
-
-
+print(queries.fetch_user_by_id('1'))
 bot.start()
